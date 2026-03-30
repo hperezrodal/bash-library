0c38280926394ba99f9c90d7173a4279014b5aa5
hperezrodal <hperezrodal@gmail.com>
2026-03-30 11:23:12 -0300

    chore: bump version to v0.3.0

--- lib-loader.sh
+++ lib-loader.sh
@@ -7,7 +7,7 @@
 LIB_DIR="${BASH_LIBRARY_PATH:+${BASH_LIBRARY_PATH}/modules}"
 LIB_DIR="${LIB_DIR:-$(dirname "${BASH_SOURCE[0]}")/modules}"
 
-export BASH_LIBRARY_VERSION="0.2.0"
+export BASH_LIBRARY_VERSION="0.3.0"
 
 # Source each module — core utilities
 # shellcheck disable=SC1091
